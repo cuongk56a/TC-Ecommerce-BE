23a5e839245c413a7b61839e9fc7563dd98df296
cuongnv108 <lymphot@example.com>
2024-04-06 08:35:05 +0700

    m

--- src/modules/role/role.model.ts
+++ src/modules/role/role.model.ts
@@ -7,39 +7,34 @@ import {getImageUriFromFilename} from '../../utils/stringUtil';
 import { hashPassword } from '../../utils/hashUtil';
 import { genCode } from '../../utils/core/genCode';
 import { TABLE_USER } from '../user/user.configs';
+import { TABLE_ORGANIZATION } from '../organization/organization.configs';
 
 export interface IRoleModelDoc extends IRoleDoc {}
 interface IRoleModel extends IDocModel<IRoleModelDoc> {}
 
 const roleSchema = new mongoose.Schema<IRoleModelDoc>(
   {
+    targetId: {
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: 'targetOneModel',
+      required: true,
+    },
+    targetOnModel: {
+      type: String,
+      enum: [TABLE_ORGANIZATION],
+      default: TABLE_ORGANIZATION
+    },
     name: {
       type: String,
-      require: true,
+      required: true,
     },
-    hotline: {
-      type: String,
-      required: false,
-      index: {unique: true, sparse: true},
+    permissions: {
+      type: [String],
+      default: []
     },
-    email: {
-      type: String,
-      required: false,
-      index: {unique: true, sparse: true},
-    },
-    webUrl: {
-      type: String,
-    },
-    thumbnail: {
-      type: String,
-      required: true
-    },
-    banner: {
-      type: String,
-    },
-    address: {
-      type: String,
-      required: false,
+    isActice: {
+      type: Boolean,
+      default: true,
     },
     createdById: {
       type: mongoose.Schema.Types.ObjectId,
@@ -65,39 +60,7 @@ const roleSchema = new mongoose.Schema<IRoleModelDoc>(
 roleSchema.plugin(toJSON);
 roleSchema.plugin(paginate);
 
-// RoleSchema.virtual('avatarUri').get(function () {
-//   return getImageUriFromFilename(this.avatar);
-// });
-
-function formatPhoneNumber(phoneNumber: any) {
-  const numericPhoneNumber = phoneNumber.replace(/\D/g, '');
-
-  if (numericPhoneNumber.startsWith('0')) {
-    const formattedPhoneNumber = `+84${numericPhoneNumber.substr(1)}`;
-    return formattedPhoneNumber;
-  } else {
-    return `+${numericPhoneNumber}`;
-  }
-}
-
-roleSchema.pre('save', async function (next) {
-  // console.log(getNewToken({id: this._id}));
-  next();
-});
-
-function preUpdate() {
-  const { phone } = this.getUpdate();
-  if (!!phone) {
-    this.getUpdate().$set.phone = formatPhoneNumber(phone);
-  }
-}
-
-roleSchema.pre('findOneAndUpdate', preUpdate);
-
-// RoleSchema.index({CODE: 1});
-roleSchema.index({phone: 1});
-roleSchema.index({email: 1});
-roleSchema.index({fullName: 'text'});
+roleSchema.index({name: 'text'});
 
 /**
  * @typedef Role
