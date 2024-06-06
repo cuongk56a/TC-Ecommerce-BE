d566e0139d876bbb6b3ada855f9493129a39d331
DESKTOP-IK35GA7\Admin <lymphot>
2024-06-07 00:55:22 +0700

    a

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -108,7 +108,7 @@ const forgotPassword = catchAsync(async (req: Request, res: Response, next: Next
   try {
     const [confirmCode, newPassword] = await Promise.all([getRedisCode(email), genCode(4)]);
     if (confirmCode !== code) {
-      res.status(httpStatus.BAD_REQUEST).send({code: httpStatus.BAD_REQUEST, status: 'Error', message: 'Mã xác thực không đúng!'});
+      res.status(httpStatus.OK).send({code: httpStatus.BAD_REQUEST, status: 'Error', message: 'Mã xác thực không đúng!'});
     } else {
       const hashedPassword = await hashPassword('newPassword');
       await userService.updateOne(
